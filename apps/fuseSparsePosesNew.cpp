3301a5c5ade07c12b47d21c6ca29c9d5b54ee7b2
Murray Evans <greenmuzz@googlemail.com>
2021-11-26 13:56:46 +0000

    We _can_ do fusion again...
    ... but there's some NaNs that need fixing

--- apps/fuseSparsePosesNew.cpp
+++ apps/fuseSparsePosesNew.cpp
@@ -4,10 +4,19 @@
 
 #include "renderer2/basicRenderer.h"
 
+#include <fstream>
 #include <iostream>
 using std::cout;
 using std::endl;
 
+
+#ifdef USE_EZC3D
+#include <ezc3d/ezc3d.h>
+#include <ezc3d/Header.h>
+#include <ezc3d/Data.h>
+#include <ezc3d/Parameter.h>
+#include <ezc3d/Frame.h>
+#endif
 
 //
 // The job of this program is to load up sparse pose detections from 
@@ -53,11 +62,19 @@ struct SData
 	
 	
 	int firstFrame;
-	
 	bool visualise;
 	
+	// cross camera associations and person tracks.
 	std::string assocFile;
+	
+	// place where we write the final 3D output.
+	std::string reconDir;
+	
+	bool writeToC3D;
+	int mocapOffset;
 };
+
+void SaveBody( std::map< int, PersonPose3D > &track, int trackNo, SData &data );
 
 void ParseConfig( std::string cfgFile, SData &data )
 {
@@ -166,9 +183,36 @@ void ParseConfig( std::string cfgFile, SData &data )
 			data.visualise = cfg.lookup("visualise");
 		}
 		
+		std::stringstream ass;
+		ass << data.dataRoot << "/" << data.testRoot << "/" << (const char*) cfg.lookup("assocFile");
+		data.assocFile = ass.str();
+		
 		std::stringstream oss;
-		oss << data.dataRoot << "/" << data.testRoot << "/" << (const char*) cfg.lookup("assocFile");
-		data.assocFile = oss.str();
+		oss << data.dataRoot << "/" << data.testRoot << "/" << (const char*) cfg.lookup("reconDir");
+		data.reconDir = oss.str();
+		
+		if( cfg.exists("saveC3D") && (bool)cfg.lookup("saveC3D"))
+		{
+			data.writeToC3D = true;
+			
+			if( cfg.exists("C3DOffsetFile") )
+			{
+				std::string s = data.dataRoot + data.testRoot + (const char*)cfg.lookup("C3DOffsetFile");
+				
+				std::ifstream infi(s);
+				std::string x;
+				infi >> x;
+				if( x.find("extra") == std::string::npos )
+					infi >> data.mocapOffset;
+				else
+					data.mocapOffset = 0;
+			}
+			else
+			{
+				data.mocapOffset = 0;
+			}
+			
+		}
 		
 	}
 	catch( libconfig::SettingException &e)
@@ -304,6 +348,7 @@ int main( int argc, char* argv[] )
 		std::map< int, PersonPose3D > fusedFrames;
 		for( auto fi = tracks[tc].begin(); fi != tracks[tc].end(); ++fi )
 		{
+			cout << tc << " " << fi->first << endl;
 			//
 			// We can initialise a PersonPose3D with links to the per-cam data.
 			//
@@ -332,4 +377,229 @@ int main( int argc, char* argv[] )
 	//
 	// And now we can dump our output.
 	//
+	for( unsigned tc = 0; tc < tracks.size(); ++tc )
+	{
+		SaveBody( tracksFused[tc], tc,  data );
+	}
+}
+
+
+void SaveBody( std::map< int, PersonPose3D > &track, int trackNo, SData &data )
+{
+	std::stringstream tss;
+	tss << data.reconDir << "/body-" << std::setw(2) << std::setfill('0') << trackNo << ".trk";
+	
+	std::stringstream css;
+	css << data.reconDir << "/body-" << std::setw(2) << std::setfill('0') << trackNo << ".c3d";
+	
+	boost::filesystem::path p(data.reconDir);
+	if( !boost::filesystem::exists(p) )
+	{
+		boost::filesystem::create_directories(p);
+	}
+	else
+	{
+		if( !boost::filesystem::is_directory(p) )
+		{
+			throw std::runtime_error("Output location exists, but is not a directory.");
+		}
+	}
+	
+	// basic text file output
+	std::ofstream outfi( tss.str() );
+	if( outfi )
+		cout << "Could not open : " << tss.str() << " for output." << endl;
+	
+	outfi << data.testRoot << endl;
+	
+	// iterate over frames...
+	for( auto fi = track.begin(); fi != track.end(); ++fi )
+	{
+		outfi << fi->first << "\t";
+		for( auto ji = fi->second.joints.begin(); ji != fi->second.joints.end(); ++ji )
+		{
+			outfi << ji->first << " " << ji->second.transpose() << "\t";
+		}
+		outfi << endl;
+	}
+	
+	outfi.close();
+	
+	//
+	// Or better yet, output to a .c3d file. Which might mean we have a slight time offset to consider,
+	// so that our .c3d file lines up with any marker-based mocap data we have.
+	//
+	if( data.writeToC3D )
+	{
+#ifdef USE_EZC3D
+		ezc3d::c3d c3d;
+		
+		ezc3d::ParametersNS::GroupNS::Parameter pointRate("RATE");
+		pointRate.set(std::vector<double>({200.0}), {1});
+		c3d.parameter("POINT", pointRate);
+		
+		
+		int numPoints = 0;
+		std::map<int, std::string> pointNames;
+		if( data.skelType == SKEL_OPOSE )
+		{
+			pointNames[17] = "rEar";       pointNames[18] = "lEar";
+			  pointNames[15] = "rEye";  pointNames[16] = "lEye";
+			               pointNames[0] = "nose";
+			               pointNames[1] = "neck";
+			pointNames[2] = "rShoulder";   pointNames[5] = "lShoulder";
+			pointNames[3] = "rElbow";      pointNames[6] = "lElbow";
+			pointNames[4] = "rWrist";      pointNames[7] = "lWrist";
+			               pointNames[8] = "midHip";
+			pointNames[9] = "rHip";        pointNames[12] = "lHip";
+			pointNames[10] = "rKnee";      pointNames[13] = "lKnee";
+			pointNames[11] = "rAnkle";     pointNames[14] = "lAnkle";
+			pointNames[24] = "rHeel";      pointNames[21] = "lHeel";
+			pointNames[22] = "rBToe";      pointNames[19] = "lBToe";
+			pointNames[23] = "rLToe";      pointNames[20] = "lLToe";
+		}
+		else if( data.skelType == SKEL_APOSE )
+		{
+			pointNames[16] = "rEar";       pointNames[17] = "lEar";
+			  pointNames[14] = "rEye";  pointNames[15] = "lEye";
+			               pointNames[0] = "nose";
+			               pointNames[1] = "neck";
+			pointNames[2] = "rShoulder";   pointNames[5] = "lShoulder";
+			pointNames[3] = "rElbow";      pointNames[6] = "lElbow";
+			pointNames[4] = "rWrist";      pointNames[7] = "lWrist";
+
+			pointNames[8] = "rHip";        pointNames[11] = "lHip";
+			pointNames[9] = "rKnee";       pointNames[12] = "lKnee";
+			pointNames[10] = "rAnkle";     pointNames[13] = "lAnkle";
+		}
+		else if( data.skelType == SKEL_DLCUT )
+		{
+			               pointNames[13] = "forehead";
+			               pointNames[12] = "chin";
+			pointNames[8] = "rShoulder";   pointNames[9]  = "lShoulder";
+			pointNames[7] = "rElbow";      pointNames[10] = "lElbow";
+			pointNames[6] = "rWrist";      pointNames[11] = "lWrist";
+
+			pointNames[2] = "rHip";        pointNames[3]  = "lHip";
+			pointNames[1] = "rKnee";       pointNames[4]  = "lKnee";
+			pointNames[0] = "rAnkle";      pointNames[5]  = "lAnkle";
+		}
+		
+		for( auto pi = pointNames.begin(); pi != pointNames.end(); ++pi )
+		{
+			c3d.point(pi->second);
+		}
+		
+		auto names = c3d.pointNames();
+		std::map<std::string, int> name2idx;
+		for( int nc = 0; nc < names.size(); ++nc )
+		{
+			cout << nc << " " << names[nc] << endl;
+			name2idx[ names[nc] ] = nc;
+		}
+		
+		//
+		// If the mocap offset is positive, we need to add in some empty frames.
+		//
+		cout << "--- empty frames for positive offset (tagA) --- " << endl;
+		int fc = 0;
+		while( fc <= data.mocapOffset )
+		{
+			//
+			// "empty" frame - a point residual of -1.0 says invalid data.
+			// but we still must specify a point for all pointNames.
+			//
+			ezc3d::DataNS::Points3dNS::Points pts;
+			for( unsigned pc = 0; pc < pointNames.size(); ++pc )
+			{
+				ezc3d::DataNS::Points3dNS::Point pt;
+				pt.x( 0.0f );
+				pt.y( 0.0f );
+				pt.z( 0.0f );
+				pt.residual(-1.0f);
+				
+				pts.point(pt, name2idx[ pointNames[pc] ] );
+			}
+			
+			ezc3d::DataNS::Frame f;
+			f.add(pts);
+			c3d.frame(f);
+			
+			cout << fc << endl;
+			++fc;
+		}
+		
+		// add a mocap frame for all video frames.
+		cout << "--- all video frames (tagB) ---" << endl;
+		for( int fc2 = 0; fc2 < track.rbegin()->first; ++fc2 )
+		{
+			if( fc2 + data.mocapOffset < 0 )
+			{
+				cout << fc << " " << fc2 << " skip ( " << fc2 + data.mocapOffset << " ) " << endl;
+				continue;
+			}
+			
+			auto fi = track.find( fc2 );
+			if( fi == track.end() )
+			{
+				// "empty" frame needed.
+				ezc3d::DataNS::Points3dNS::Points pts;
+				for( unsigned pc = 0; pc < pointNames.size(); ++pc )
+				{
+					ezc3d::DataNS::Points3dNS::Point pt;
+					pt.x( 0.0f );
+					pt.y( 0.0f );
+					pt.z( 0.0f );
+					pt.residual(-1.0f);
+					
+					pts.point(pt, name2idx[ pointNames[pc] ] );
+				}
+				
+				ezc3d::DataNS::Frame f;
+				f.add(pts);
+				c3d.frame(f);
+				cout << fc << " " << fc2 << " empty " << endl;
+				++fc;
+			}
+			else
+			{
+				// fill a frame structure
+				ezc3d::DataNS::Frame f;
+				ezc3d::DataNS::Points3dNS::Points pts;
+				for( unsigned pc = 0; pc < pointNames.size(); ++pc )
+				{
+					ezc3d::DataNS::Points3dNS::Point pt;
+					
+					auto ji = fi->second.joints.find( pc );
+					if( ji != fi->second.joints.end() )
+					{
+						pt.x( ji->second(0) );
+						pt.y( ji->second(1) );
+						pt.z( ji->second(2) );
+						pt.residual(1.0f);
+					}
+					else
+					{
+						pt.x( 0.0f );
+						pt.y( 0.0f );
+						pt.z( 0.0f );
+						pt.residual(-1.0f);
+					}
+					
+					pts.point(pt, name2idx[ pointNames[pc] ] );
+				}
+				cout << fc << " " << fc2 << " " << pts.nbPoints() << endl;
+				f.add(pts);
+				// add the frame to the c3d file
+				c3d.frame(f);
+				++fc;
+			}
+		}
+		
+		c3d.write( css.str() );
+		
+#else
+		cout << "you asked to write to c3d, but I was compiled without ezc3d support :( " << endl;
+#endif
+	}
 }
